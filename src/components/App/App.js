1403b395fa9c35677dd4490ac9c030631975b62f
nguyenduythinh.95@gmail.com <wolfgang25th@gmail.com>
2023-12-20 06:03:31 -0600

    Update Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer.js";
 import ModalWithForm from "../ModalWithForm/ModalwithForm";
 import { useEffect, useState } from "react";
 import ItemModal from "../ItemModal/ItemModal";
